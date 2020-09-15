c1b303cb101d0380abb0ca0c447fc25fb7671e75
royeradames <royeraadames@gmail.com>
2020-09-15 17:24:42 -0400

    fix migration rollback error. inverted table drop order

--- database/migrations/20180910095022_init.js
+++ database/migrations/20180910095022_init.js
@@ -1,25 +1,21 @@
 exports.up = function (knex) {
   return knex.schema
     .createTable("roles", tbl => {
-      tbl.increments();
+      tbl.increments()
 
-      tbl.string("name", 128).notNullable().unique();
+      tbl.string("name", 128).notNullable().unique()
     })
     .createTable("users", tbl => {
-      tbl.increments();
+      tbl.increments()
 
-      tbl.string("username", 128).notNullable().unique().index();
-      tbl.string("password", 256).notNullable();
+      tbl.string("username", 128).notNullable().unique().index()
+      tbl.string("password", 256).notNullable()
 
       tbl
-        .integer("role")
-        .unsigned()
-        .references("roles.id")
-        .onDelete("RESTRICT")
-        .onUpdate("CASCADE");
-    });
-};
+        .string("role").index()
+    })
+}
 
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists("roles").dropTableIfExists("users");
-};
+  return knex.schema.dropTableIfExists("users").dropTableIfExists("roles")
+}
